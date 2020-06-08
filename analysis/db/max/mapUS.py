502d8c26684c6366c8bfdeef7cab323e9d0b9e5b
Makhsuda <makhsuda@vt.edu>
2020-06-08 16:01:10 -0400

    add data for map with latitude and longitude

--- analysis/db/max/mapUS.py
+++ analysis/db/max/mapUS.py
@@ -2,6 +2,12 @@ import geopandas as gpd
 from shapely.geometry import Point, Polygon
 import matplotlib.pyplot as plt
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 
-usa = gpd.read_file("./tl_2017_us_state/tl_2017_us_state.shp")
-usa.plot()
+
+loc_df = pd.read_csv('us-zip-code-latitude-and-longitude.csv', index_col=0)
+print(loc_df.head())
+fig = px.scatter_mapbox(loc_df, lat="lat", lon="lon", color="State")
+fig.update_layout(mapbox_style="open-street-map")
+fig.show()
