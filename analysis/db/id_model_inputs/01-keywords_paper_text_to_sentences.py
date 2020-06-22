a0854087cd504be8b6979f2b072fa1f5fa5102dd
Daniel Chen <chendaniely@gmail.com>
2020-06-22 10:57:10 -0400

    look for sentences that match set of words

--- analysis/db/id_model_inputs/01-keywords_paper_text_to_sentences.py
+++ analysis/db/id_model_inputs/01-keywords_paper_text_to_sentences.py
@@ -1,67 +1,96 @@
 import itertools
 import pathlib as pl
+import re
+import sys
 
 import nltk.data
 from nltk.tokenize import RegexpTokenizer
 import pandas as pd
-from tqdm import tqdm
-tqdm.pandas()
-
 from pyprojroot import here
+from tqdm import tqdm
+
+print('version2')
+
+# def find_pattern_in_list(sent, pattern):
+#     return [bool(re.search(pattern, x)) for x in sent]
 
 
-def has_search_terms(dat, term, text_col):
-    term_sentences = dat[text_col].str.contains(term, case=False, regex=True)
-    new_term_string = term.replace(" ", "_")
-    dat[f"has-{new_term_string}"] = term_sentences
-    return(dat)
+# def has_search_terms(dat, term, pattern, sent_col):
+#     term_sentences = dat[sent_col].progress_apply(find_pattern_in_list, pattern=pattern)
+#     new_term_string = term.replace(" ", "_")
+#     dat[f"has-{new_term_string}"] = any(term_sentences)
+#     dat[f"found-{new_term_string}"] = term_sentences
+#     return(dat)
 
-def convert_terms_to_regex(term):
+
+def convert_term_to_regex(term):
     words = term.split(" ")
-    regex_pattern = "^"
+    regex_pattern = ""
     for term in words:
-        regex_pattern += f"(?=.*{term}\b)"
-    regex_pattern += ".*$"
+        regex_pattern += f"(?=.*\\b{term}\\b)"
+    regex_pattern += ""
     return regex_pattern
+assert convert_term_to_regex("incubation period") == "(?=.*\\bincubation\\b)(?=.*\\bperiod\\b)"
 
-assert convert_terms_to_regex("incubation period") == "^(?=.*incubation\b)(?=.*period\b).*$"
 
-paper_df = pd.read_csv(here() / "data" / "db" / "final" / "kaggle" / "paper_text" / "document_parses_pmc_json.tsv", sep = "\t")
+def find_matches_in_list(sentences, terms):
+    """Determins if a term exists in a list of sentences
+    """
+    matches = {}
+    for trm in terms:
+        #breakpoint()
+        pattern = convert_term_to_regex(trm)
+        for sent in sentences:
+            if (re.search(pattern, sent)):
+                matches[trm] = True
+                break # if there is a pattern match go to the next term
+    return(matches)
+assert find_matches_in_list(['hello my name is dan', 'hello you', 'the quick brown fox'],
+                     ['hello dan', 'fox', 'tom nook']) == {
+                         'hello dan': True,
+                         'fox': True
+                     }
 
-# can also load within the here function
-# paper_df = pd.read_csv(here("./data/db/final/kaggle/paper_text/document_parses_pmc_json.tsv"), sep = "\t")
 
+script = sys.argv[0]
+try:
+    flag = sys.argv[1]
+except IndexError:
+    flag = ''
+
+if flag == '--test':
+    print("running test")
+    paper_df = pd.read_csv(here("./data/db/final/kaggle/paper_text/document_parses_pmc_json.tsv"),
+                           sep = "\t",
+                           nrows=10)
+else :
+    paper_df = pd.read_csv(here("./data/db/final/kaggle/paper_text/document_parses_pmc_json.tsv"),
+                           sep = "\t")
+
+# break up into sentences
+sent_detector = nltk.data.load('tokenizers/punkt/english.pickle') # nltk.download('punkt')
+tqdm.pandas(desc="Split sentences")
+paper_df["text_sent_lower"] = paper_df["text"].progress_apply(lambda x: sent_detector.tokenize(x.lower().strip()))
+
+# search for terms
 search_terms = [
     "incubation period",
     "infectiousness period",
     "recovery rate",
     "case fatality ratio",
+    "case fatality rate",
     "asymptomatic fraction",
-    "hospitalized fraction"
+    "asymptomatic proportion",
+    "asymptomatic ratio",
+    "hospitalized fraction",
+    "hospitalized proportion",
+    "latent period",
 ]
 
-search_terms_regex = [convert_terms_to_regex(x) for x in search_terms]
-
-has_terms_df = paper_df
-
-for term in tqdm(search_terms):
-    has_terms_df = has_search_terms(has_terms_df, term, 'text')
-
-# tablulate term results
-print(has_terms_df
-     .filter(like="has-", axis='columns')
-     .apply(lambda x: x.value_counts(dropna=False))
-     .assign(count=lambda x: x.apply('sum', axis='columns'))
-)
-
-# break up into sentences
-#import nltk
-#nltk.download('punkt')
-sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
-
-has_terms_df["text_sent"] = has_terms_df["text"].progress_apply(lambda x: sent_detector.tokenize(x.strip()))
-
+tqdm.pandas(desc="Finding terms")
+paper_df['found_terms'] = paper_df['text_sent'].progress_apply(find_matches_in_list, terms=search_terms)
 
 # save out working data
-pl.Path(here("./data/db/working/kaggle/id_model_inputs", warn=False)).mkdir(parents=True, exist_ok=True)
-has_terms_df.to_pickle(here("./data/db/working/kaggle/id_model_inputs/01-sentences-keywords.pickle", warn=False))
+if flag != '--test':
+    pl.Path(here("./data/db/working/kaggle/id_model_inputs", warn=False)).mkdir(parents=True, exist_ok=True)
+    paper_df.to_pickle(here("./data/db/working/kaggle/id_model_inputs/01-sentences-keywords-2.pickle", warn=False))
