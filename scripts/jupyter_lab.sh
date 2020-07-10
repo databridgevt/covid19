f53459e65102ef8fd9e806252df947e2a1a55c6b
Daniel Chen <chendaniely@gmail.com>
2020-07-10 13:04:13 -0400

    script for plotly and dask rendering in jupyter lab

---
+++ scripts/jupyter_lab.sh
@@ -0,0 +1,7 @@
+# setup dask extension
+jupyter labextension install dask-labextension
+jupyter serverextension enable dask_labextension
+
+# setup plotly
+jupyter labextension install jupyterlab-plotly@4.8.2
+jupyter labextension install @jupyter-widgets/jupyterlab-manager plotlywidget@4.8.2
