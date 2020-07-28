609d480be7e15eeef7a7681025c718b586d46faa
Makhsuda <makhsuda@vt.edu>
2020-07-28 19:44:58 -0400

    remove dataset with fips codes, rebuild the map using only one dataset

--- analysis/db/us_map/app.py
+++ analysis/db/us_map/app.py
@@ -11,59 +11,39 @@ import json
 with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
     counties = json.load(response)
 
+# TODO: color "Great Salt Lake" in Utah state
+
 external_stylesheets = [here('./analysis/db/us_map/assets/style.css')]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, suppress_callback_exceptions=True)
 
 confirmed_df = pd.read_csv('https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/'
                            'csse_covid_19_time_series/time_series_covid19_confirmed_US.csv')
-# Resource for State_FIPS: https://www.nrcs.usda.gov/wps/portal/nrcs/detail/national/home/?cid=nrcs143_013697
-loc_df = pd.read_excel(here('./data/db/original/maps/State_FIPS.xlsx'))
 # Resource for PopulationEstimates: https://www.ers.usda.gov/data-products/county-level-data-sets/download-data/
 pop_df = pd.read_excel(here('./data/db/original/maps/PopulationEstimates.xls'))  # population dataset for 2019
 
 pop_df['fips_str'] = pop_df['FIPStxt'].apply(lambda x: f'{x:05.0f}')
-pop_df = pop_df[['fips_str', 'Area_Name', 'POP_ESTIMATE_2019']]
+confirmed_df['fips_str'] = confirmed_df['FIPS'].apply(lambda x: f'{x:05.0f}')
 
-merged_df = pd.merge(loc_df, confirmed_df, right_on='Admin2', left_on='Name')
+pop_short_df = pop_df[['fips_str', 'Area_Name', 'POP_ESTIMATE_2019']]  # shorten columns in population dataset
 
-merged_df['fips_str'] = merged_df['FIPS_x'].apply(lambda x: f'{x:05.0f}')  # left pad with 0 for 5 digits
+merged_df = pd.merge(confirmed_df, pop_short_df, right_on='fips_str', left_on='fips_str')
+
 molten_df = merged_df.melt(
-    id_vars=['FIPS_x', 'Name', 'State', 'UID', 'iso2', 'iso3', 'code3', 'FIPS_y', 'Admin2',
-             'Province_State', 'Country_Region', 'Lat', 'Long_', 'Combined_Key', 'fips_str'],
+    id_vars=['UID', 'iso2', 'iso3', 'code3', 'FIPS', 'Admin2',
+             'Province_State', 'Country_Region', 'Lat', 'Long_', 'Combined_Key',
+             'fips_str', 'Area_Name', 'POP_ESTIMATE_2019'],
     var_name=['date']
 )
+molten_df['date_iso'] = pd.to_datetime(molten_df['date'], format="%m/%d/%y")  # format date as 'yyyy-mm-dd'
 
-molten_df['date_iso'] = pd.to_datetime(molten_df['date'], format="%m/%d/%y")  # change date to ISO8601 standard format
+molten_df['fips_str'] = molten_df['fips_str'].replace(['46102'], '46113')  # replace new fips code with old one
 
-molten_pop_df = pd.merge(molten_df, pop_df, on='fips_str')  # add population per county
-grouped_by_df = molten_pop_df.groupby(['fips_str', 'date_iso', 'State', 'Admin2', 'POP_ESTIMATE_2019'])[
+grouped_by_df = molten_df.groupby(['fips_str', 'date_iso', 'Admin2', 'POP_ESTIMATE_2019'])[
     'value'].sum().reset_index()
+
 grouped_by_df['total_per_cap'] = grouped_by_df['value'] / grouped_by_df['POP_ESTIMATE_2019']  # get per capita value
-date_string = '2020-06-01'
 
-# plot_data = grouped_by[grouped_by.date_iso == date_string]  # confirmed cases on a specific day
-
-value = 'value'  # 'value' = raw count, 'total_per_cap' = per capita
-
-# confirmed cases per capita/raw count
-'''
-fig = px.choropleth(plot_data,
-                    geojson=counties,
-                    locations=plot_data.fips_str,
-                    color=value,
-                    hover_data=['State', 'Admin2', value, 'POP_ESTIMATE_2019'],
-                    color_continuous_scale='viridis_r',
-                    range_color=(0, plot_data[value].max()),
-                    scope="usa",
-                    # title='Confirmed cases',
-                    labels={'value': 'confirmed cases'}
-                    )
-
-fig.update_layout(
-    template="plotly_dark"
-)
-'''
 app.layout = html.Div(children=[
     html.Header(className='header',
                 children=[
@@ -124,7 +104,7 @@ app.layout = html.Div(children=[
              ]),
     html.Footer(
         children=[
-            html.P('''All rights reserved''')
+            html.P("""All rights reserved""")
         ])
 ])
 
@@ -176,7 +156,7 @@ def update_figure(date):
                         geojson=counties,
                         locations=plot_data.fips_str,
                         color=value,
-                        hover_data=['State', 'Admin2', value, 'POP_ESTIMATE_2019'],
+                        hover_data=['Admin2', value, 'POP_ESTIMATE_2019'],
                         color_continuous_scale='viridis_r',
                         range_color=(0, plot_data[value].max()),  # plot_data[value].max()
                         scope="usa",
@@ -184,7 +164,7 @@ def update_figure(date):
                         labels={'value': 'confirmed cases'}
                         )
     fig.update_layout(
-        template="plotly_dark"
+        template="gridon"
     )
     return fig
 
